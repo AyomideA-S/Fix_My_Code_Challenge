770a1c5f358bd2d3e9805d17999a6799f5c4bef6
Ayomide Ayodele-Soyebo <61430633+AyomideA-S@users.noreply.github.com>
2022-12-06 22:47:51 +0000

    Update 2-sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -20,14 +20,12 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
     end
     result << i_arg if !is_inserted
 end
-
-result = result.sort()
 
 puts result
