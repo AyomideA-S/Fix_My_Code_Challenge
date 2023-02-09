9be3e623b6ea334d5e72c4ed0b374233e85e28ba
Ayomide Ayodele-Soyebo <midesuperbest@gmail.com>
2023-02-09 02:11:48 +0100

    Documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
+""" Square class """
 
-class square():
+class Square():
+
+    """ This is the Square class with 2 variables: width & height """
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Initializer """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """ Function to find the perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ This is the printing function for the Square class """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
